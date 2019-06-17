2589f60471b57ad6c2d158bffbd44387fe526380
reggielee <reggielee107@gmail.com>
2019-06-17 17:22:02 +0800

    feat&fixed: Add webpack and fix the style problem
    add webpack&babel,and fix the style problem in todo item
    
    author:reggielee

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
 
 
 module.exports = {
-    entry:'./src/index.js',
-    output:{
-        filename:'bundle.js',
-        path:path.resolve(__dirname,'dist')
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js',
+        path: path.resolve(__dirname, 'dist')
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './public/index.html'
+        }),
+        new CleanWebpackPlugin()
+    ],
     devtool: 'eval-source-map',
-    // module:{
-    //     rules:[{
-    //         test://
-    //     }]
-    // }
+    devServer: {
+        port: 8011,
+        compress: true,
+        contentBase: path.join(__dirname, 'dist')
+    }
 }
